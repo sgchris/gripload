b29cebc61a31f1828b2e727966f9226508be389d
Gregory C <sgchris@gmail.com>
2014-09-14 08:39:45 +0300

    created stored inputs object inside

--- gripload.js
+++ gripload.js
@@ -15,18 +15,23 @@ var gripload = (function() {
 		// return merged object. obj2 is injected into obj1
 		mergeObjects: function(obj1, obj2) {
 			for (var attr in obj2) {
-				obj1[attr] = obj[attr];
+				obj1[attr] = obj2[attr];
 			}
 
 			return obj1;
 		}
 	};
 
-	var mainFn = function (fileInput, options) {
+	var uploadFiles = function(evt) {
+		var theInput = evt.target;
+		if (!theInput || theInput.tagName !== 'INPUT') {
+			return false;
+		}
+		/*
 		var files = fileInput.files;
 		if (!files || !files.length) return; 
 
-		for (var i = 0, filesCount = files.length, file = files[i]; i < filesCount; i++) {
+		for (var i = 0; file = files[i++];) {
 			var reader  = new FileReader();
 			reader.onloadend = function() {
 				console.log('size', reader.result.length);
@@ -34,6 +39,47 @@ var gripload = (function() {
 			};
 			reader.readAsBinaryString(file);
 		}
+		*/
+	};
+
+	var fileInputs = (function() {
+		var storedInputs = [];
+
+		return {
+			// add the input to the list and assign 'change' event
+			addAndAssign: function(fInput, options) {
+				fInput.addEventListener('change', uploadFiles);
+				storedInputs.push({
+					'input': fInput, 
+					'options': options
+				});
+			},
+
+			// check if the input is already inside
+			exists: function(fInput) {
+				var found = false;
+				storedInputs.every(function(fInput) {
+					if (storedInputs.input === fInput) {
+						found = true;
+						return false;
+					}
+				});
+
+				return found;
+			}
+		};
+	})();
+
+	var mainFn = function (fileInput, options) {
+
+		// merge with default
+		options = tools.mergeObjects(defaultOptions, options);
+		
+		// register the file input
+		if (!fileInputs.exists(fileInput)) {
+			fileInputs.addAndAssign(fileInput, options);
+		}
+
 	};
 
 	return mainFn;
